c03c8e40736701c64b9bb8912e015993c63acf3a
Eriton <botheritons2005@gmail.com>
2024-09-24 09:07:32 -0300

    feat: add AuthController methods

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -2,10 +2,9 @@
 
 namespace App\Http\Controllers\Auth;
 
-use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Http\Controllers\AuthController as ControllersAuthController;
 
-class AuthController extends Controller
+class AuthController extends ControllersAuthController
 {
     public function login() {
 
