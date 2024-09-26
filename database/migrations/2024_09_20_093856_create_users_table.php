19ee80212870d2440c7ac589834c27c320f49e27
Eriton <botheritons2005@gmail.com>
2024-09-26 09:44:05 -0300

    fix: add collumn password

--- database/migrations/2024_09_20_093856_create_users_table.php
+++ database/migrations/2024_09_20_093856_create_users_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('username', 24);
+            $table->string('password');
             $table->string('email', 255)->unique();
             $table->timestamps();
         });
