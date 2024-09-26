ce9c0d97c07e0f07082b48eac679a47aa96090c2
Eriton <botheritons2005@gmail.com>
2024-09-26 08:29:55 -0300

    feat: add method to formatted response with user params

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,7 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\User\UserResource;
+
 abstract class Controller
 {
-    //
+
+    public function respondWithUser(
+        $message = null,
+        $httpCode = 200,
+        UserResource $user) {
+        return response()->json([
+            'message' => $message,
+            'status' => $httpCode,
+            'user' => $user,
+        ], $httpCode);
+    }
 }
