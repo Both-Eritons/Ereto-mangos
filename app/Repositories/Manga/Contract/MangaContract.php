27e9712a9847fefd5dffa454355d19155fe5303e
Eriton <botheritons2005@gmail.com>
2024-09-30 07:27:05 -0300

    feat: adding methods to implement in the repository

--- app/Repositories/Manga/Contract/MangaContract.php
+++ app/Repositories/Manga/Contract/MangaContract.php
@@ -4,5 +4,7 @@ namespace App\Repositories\Manga\Contract;
 
 interface MangaContract {
     public function createManga(array $data);
-
+    public function findMangaByTitle(string $title);
+    public function findMangaById(int $id);
+    public function findMangasByAuthor(string $author);
 }
