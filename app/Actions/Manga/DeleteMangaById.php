b311fda3b1757d534bfeff7bb0d1a1ff6fd2bdfd
Eriton <botheritons2005@gmail.com>
2024-10-09 10:42:58 -0300

    feat: Delete Manga By ID action

--- app/Actions/Manga/DeleteMangaById.php
+++ app/Actions/Manga/DeleteMangaById.php
@@ -3,6 +3,7 @@ declare(strict_types=1);
 
 namespace App\Actions\Manga;
 
+use App\Exceptions\Manga\DeleteMangaException;
 use App\Exceptions\Manga\MangaNotExistsException;
 use App\Repositories\Manga\MangaRepository;
 
@@ -13,13 +14,17 @@ class DeleteMangaById
     {}
 
 
-    public function execute(int $id)
+    public function execute(int $id): array
     {
         $manga = $this->manga->findMangaById($id);
 
         if(!$manga) throw new MangaNotExistsException();
 
+        $manga = $this->manga->deleteMangaById($id);
 
+        if(!$manga) throw new DeleteMangaException();
+
+        return ['message' => 'Manga Deletado com Sucesso!'];
     }
 
 }
