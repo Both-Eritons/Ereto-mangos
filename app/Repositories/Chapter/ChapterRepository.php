f2d38304a7c1a20d66d1ea452552c98a4efa222d
Eriton <botheritons2005@gmail.com>
2024-10-12 07:46:13 -0300

    feat: Chapter Repository and Contract

--- app/Repositories/Chapter/ChapterRepository.php
+++ app/Repositories/Chapter/ChapterRepository.php
@@ -2,9 +2,29 @@
 
 namespace App\Repositories\Chapter;
 
+use App\Models\Manga\Chapter;
 use App\Repositories\Chapter\Contract\ChapterContract;
 
 class ChapterRepository implements ChapterContract
 {
 
+    public function __construct(private Chapter $chapter)
+    {
+
+    }
+
+    public function create(array $array): ?Chapter
+    {
+        return $this->chapter::create($array);
+    }
+
+    public function deleteById(int $id): ?Chapter
+    {
+        return $this->chapter::where('id', $id)->delete();
+    }
+
+    public function findById(int $id): ?Chapter
+    {
+        return $this->chapter::find($id);
+    }
 }
--- app/Repositories/Chapter/Contract/ChapterContract.php
+++ app/Repositories/Chapter/Contract/ChapterContract.php
@@ -2,6 +2,10 @@
 
 namespace App\Repositories\Chapter\Contract;
 
-interface ChapterContract {
+use App\Models\Manga\Chapter;
 
+interface ChapterContract {
+    public function create(array $array): ?Chapter;
+    public function deleteById(int $int): ?Chapter;
+    public function findById(int $int): ?Chapter;
 }
