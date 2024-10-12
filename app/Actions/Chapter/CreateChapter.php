9a1e4abaf2ab768acd6c0c4901bd9c2c7b912183
Eriton <botheritons2005@gmail.com>
2024-10-12 16:53:02 -0300

    feat: init chapter upload

---
+++ app/Actions/Chapter/CreateChapter.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Actions\Chapter;
+
+use App\Repositories\Chapter\ChapterRepository;
+
+class CreateChapter
+{
+
+    public function __construct(private ChapterRepository $chapter)
+    {
+    }
+
+    public function execute(array $data) {
+        $chapter = $this->chapter->create($data);
+        return $chapter;
+    }
+}
