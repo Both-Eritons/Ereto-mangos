821bd3895d531877f164fdeb7272af6b063e7492
Eriton <botheritons2005@gmail.com>
2024-10-17 09:00:35 -0300

    feat: PermsEnum, add Find manga and chapter

--- app/Enums/Roles/PermsEnum.php
+++ app/Enums/Roles/PermsEnum.php
@@ -7,10 +7,11 @@ enum PermsEnum: string
     case CREATE_MANGA = 'create manga';
     case UPDATE_MANGA = 'update manga';
     case DELETE_MANGA = 'delete manga';
+    case FIND_MANGA = 'find manga';
 
     case CREATE_CHAPTER = 'create chapter';
     case UPDATE_CHAPTER = 'update chapter';
     case UPLOAD_CHAPTER = 'upload chapter';
     case DELETE_CHAPTER = 'delete chapter';
-
+    case FIND_CHAPTER = 'find chapter';
 }
