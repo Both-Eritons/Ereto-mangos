4cbff1d6808cb52ee10c139850a8629858bad57c
Eriton <botheritons2005@gmail.com>
2024-10-20 12:24:48 -0300

    feat: get chapters by title finished

--- app/Actions/Chapter/GetChaptersByMangaTitle.php
+++ app/Actions/Chapter/GetChaptersByMangaTitle.php
@@ -1,16 +1,24 @@
 <?php
 namespace App\Actions\Chapter;
 
+use App\Exceptions\Chapter\ChapterNotFoundException;
+use App\Exceptions\Manga\MangaNotExistsException;
 use App\Repositories\Chapter\ChapterRepository;
+use App\Repositories\Manga\MangaRepository;
 
 class GetChaptersByMangaTitle
 {
     public function __construct(
         private ChapterRepository $chapter,
+        private MangaRepository $manga,
     ){}
 
     public function execute(array $data)
     {
-
+        $manga = $this->manga->findMangaBySlug($data['title']);
+        if(!$manga) throw new MangaNotExistsException();
+        $manga = $this->chapter->getChaptersByMangaTitle($data['title']);
+        if(!$manga) throw new ChapterNotFoundException();
+        return $manga;
     }
 }
