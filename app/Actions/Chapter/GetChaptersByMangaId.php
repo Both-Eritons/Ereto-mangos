6feae8fea12e5cf7b1682768b171850348fdd080
Eriton <botheritons2005@gmail.com>
2024-10-20 07:45:18 -0300

    feat: get chapters by manga id

--- app/Actions/Chapter/GetChaptersByMangaId.php
+++ app/Actions/Chapter/GetChaptersByMangaId.php
@@ -1,16 +1,25 @@
 <?php
 namespace App\Actions\Chapter;
 
+use App\Exceptions\Chapter\ChapterNotFoundException;
+use App\Exceptions\Manga\MangaNotExistsException;
 use App\Repositories\Chapter\ChapterRepository;
+use App\Repositories\Manga\MangaRepository;
 
 class GetChaptersByMangaId
 {
     public function __construct(
         private ChapterRepository $chapter,
+        private MangaRepository $manga,
     ){}
 
     public function execute(array $data)
     {
+        $manga = $this->manga->findMangaById($data['id']);
+        if(!$manga) throw new MangaNotExistsException();
+        $manga = $this->chapter->getChaptersByMangaId($data['id']);
+        if(!$manga) throw new ChapterNotFoundException();
 
+        return $manga;
     }
 }
--- app/Http/Controllers/Chapter/ChapterController.php
+++ app/Http/Controllers/Chapter/ChapterController.php
@@ -3,16 +3,18 @@
 namespace App\Http\Controllers\Chapter;
 
 use App\Actions\Chapter\CreateChapter;
+use App\Actions\Chapter\GetChaptersByMangaId;
+use App\Actions\Chapter\GetChaptersByMangaSlug;
+use App\Actions\Chapter\GetChaptersByMangaTitle;
 use App\Http\Controllers\ChapterBase;
-use App\Http\Requests\Chapter\UploadChapterRequest as Request;
-use App\Models\Manga\Chapter;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Storage;
-use Illuminate\Support\Str;
+use App\Http\Requests\Chapter\UploadChapterRequest as UploadRequest;
+use Illuminate\Http\Request;
 
 class ChapterController extends ChapterBase
 {
-    public function postChapter(Request $req, CreateChapter $action)
+    public function postChapter(
+        UploadRequest $req,
+        CreateChapter $action)
     {
         $array = [
             'images' => $req->validated()['images'],
@@ -24,18 +26,27 @@ class ChapterController extends ChapterBase
         return response($result, 201);
     }
 
-    public function getChaptersByMangaId(Request $req)
+    public function getChaptersByMangaId(
+        Request $req,
+        GetChaptersByMangaId $action)
     {
-
+        $result = $action->execute(['id' => $req->id]);
+        return response([$result]);
     }
 
-    public function getChaptersByMangaSlug(Request $req)
+    public function getChaptersByMangaSlug(
+        Request $req,
+        GetChaptersByMangaSlug $action)
     {
-
+        $result = $action->execute(['slug' => $req->slug]);
+        return response([$result]);
     }
 
-    public function getChaptersByMangaTitle(Request $req)
+    public function getChaptersByMangaTitle(
+        Request $req,
+        GetChaptersByMangaTitle $action)
     {
-
+        $result = $action->execute(['title' => $req->title]);
+        return response($result);
     }
 }
