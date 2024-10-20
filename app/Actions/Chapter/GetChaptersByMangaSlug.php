61807616ecbfa6e23e8a921ae7bece36a7e359f5
Eriton <botheritons2005@gmail.com>
2024-10-20 12:24:35 -0300

    feat: get chapters by slug finished

--- app/Actions/Chapter/GetChaptersByMangaSlug.php
+++ app/Actions/Chapter/GetChaptersByMangaSlug.php
@@ -1,16 +1,24 @@
 <?php
 namespace App\Actions\Chapter;
 
+use App\Exceptions\Chapter\ChapterNotFoundException;
+use App\Exceptions\Manga\MangaNotExistsException;
 use App\Repositories\Chapter\ChapterRepository;
+use App\Repositories\Manga\MangaRepository;
 
 class GetChaptersByMangaSlug
 {
     public function __construct(
         private ChapterRepository $chapter,
+        private MangaRepository $manga,
     ){}
 
     public function execute(array $data)
     {
-
+        $manga = $this->manga->findMangaBySlug($data['slug']);
+        if(!$manga) throw new MangaNotExistsException();
+        $manga = $this->chapter->getChaptersByMangaSlug($data['slug']);
+        if(!$manga) throw new ChapterNotFoundException();
+        return $manga;
     }
 }
