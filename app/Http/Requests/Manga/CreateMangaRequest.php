371a9d25309ae7f74440706c2bb383e10c65213d
Eriton <botheritons2005@gmail.com>
2024-10-20 06:18:14 -0300

    feat: CreateManga FormRequest, putting portuguese messages in FormRequest error

--- app/Http/Requests/Manga/CreateMangaRequest.php
+++ app/Http/Requests/Manga/CreateMangaRequest.php
@@ -35,4 +35,22 @@ class CreateMangaRequest extends FormRequest
             'sinopse' => 'required|min:10|max:400',
         ];
     }
+
+    public function messages(): array
+    {
+        return [
+            'title.required' => 'O Titulo é obrigatorio.',
+            'title' => 'O Title precisa ser Valido.',
+            'title.min' => 'O Titulo Está muito curto.',
+            'title.max' => 'O Titulo ultrapassou o Limite.',
+
+            'sinopse.required' => 'A Sinopse é obrigatorio.',
+            'sinopse' => 'A sinopse precisa ser Valido.',
+            'sinopse.min' => 'A Sinopse Está muito curta.',
+            'sinopse.max' => 'A sinopse ultrapassou o Limite.',
+
+
+        ];
+    }
+
 }
